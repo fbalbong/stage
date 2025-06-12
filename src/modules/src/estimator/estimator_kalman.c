cf2b33c6dc2cf4f0cebfd178f9ff4610f4ef307d
fbalbong <fbalbong@gmail.com>
2025-06-12 11:48:22 +0200

    prueba 2 lighthouse fuera de correccion

--- src/modules/src/estimator/estimator_kalman.c
+++ src/modules/src/estimator/estimator_kalman.c
@@ -275,7 +275,7 @@ static void kalmanTask(void* parameters) {
   }
 }
 
-void estimatorKalman_oot(state_t *state, const stabilizerStep_t stabilizerStep) {
+void estimatorKalman(state_t *state, const stabilizerStep_t stabilizerStep) {
   // This function is called from the stabilizer loop. It is important that this call returns
   // as quickly as possible. The dataMutex must only be locked short periods by the task.
   xSemaphoreTake(dataMutex, portMAX_DELAY);
